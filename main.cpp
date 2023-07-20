d023e65c5d4f16c2d73741a8e8c800fdd12152b0
WanderingLemon <wisdomcr9@gmail.com>
2023-07-20 12:20:25 -0400

    Restoring main and adding util.h to cmake

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream> // for writing to stdio
+#include <random>
+#include <unistd.h>
 
 using namespace std;
 
@@ -6,8 +8,18 @@ int main(int argv, char** args) {
     cout << "Number of arguments input: " << argv << endl;
 
     // Goes through all arguments
-    for(int i = 0; i < argv; args++, i++ ){
-        cout <<"["<<i<<"] "<< *args << endl;
+    for(int i = 0; i < argv; args++, i++ ) {
+
     }
+
+    // RNG settings
+    random_device rd;
+    default_random_engine engine(rd());
+    uniform_int_distribution<int> distribution(100,2000);
+
+    int rand = distribution(engine);
+    cout << rand << endl;
+
+
     return 0;
 }

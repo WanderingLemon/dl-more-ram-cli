416a1023745ec8e957d23ecadbe991856557381a
WanderingLemon <wisdomcr9@gmail.com>
2023-07-20 21:54:37 -0400

    added the main RAM "loading" animation

--- Utils.cpp
+++ Utils.cpp
@@ -1,5 +1,5 @@
 //
-// Created by wisdo on 7/20/2023.
+// Created by Lemon on 7/20/2023.
 //
 
 #include "Utils.h"
@@ -7,8 +7,17 @@
 #include <random>
 #include <string>
 #include <utility>
+#include <thread>
+#include <chrono>
 
 using namespace std;
+
+const string RAM_ASCII_NP = " ____________________________________________________ \n"
+                            "|    ____      ____      ____      ____      ____    |\n"
+                            "| ..|    |  . |    |  . |    |  . |    | .  |    |  .|\n"
+                            "| . |    | .. |    | .  |    | .. |    |. . |    |.  |\n"
+                            "|. .|____| . .|____|  . |____| . .|____| .  |____| . |\n"
+                            "|____________________________________________________|\n";
 
 Utils::Utils(){
     random_device randomDevice;
@@ -39,5 +48,40 @@ Utils::Utils(int amountOfRam, string typeOfRam, char unitOfRam) {
 int Utils::rand(int lb, int ub) {
     uniform_int_distribution<int> distribution(lb,ub);
     return distribution(randEngine);
+}
+
+void Utils::loadingRAMAnim() {
+    cout << RAM_ASCII_NP;
+    char current;
+    int lb = 50;
+    int ub = 1000;
+    string pins = "||||||||||||||||  ||||||||||||||||||||||||||||||||||||"; // len = 54
+    while(!pins.empty()){
+        switch (pins.length()){
+            case 40:
+                lb = 100;
+                ub = 1500;
+                break;
+            case 30:
+                lb = 150;
+                ub = 2000;
+                break;
+            case 14:
+                lb = 200;
+                ub = 2200;
+                break;
+            case 3:
+                lb = 1000;
+                ub = 3000;
+        }
+
+        this_thread::sleep_for(chrono::milliseconds(rand(lb,ub)));
+        current = pins[0];
+        pins.erase(0,1);
+        cout << current;
+//        cout << pins << endl;
+    }
+
+
 }
 
--- Utils.h
+++ Utils.h
@@ -1,5 +1,5 @@
 //
-// Created by wisdo on 7/20/2023.
+// Created by Lemon on 7/20/2023.
 //
 #include <string>
 #include <random>
@@ -7,16 +7,26 @@
 #ifndef DL_MORE_RAM_CLI_UTILS_H
 #define DL_MORE_RAM_CLI_UTILS_H
 
+extern const std::string RAM_ASCII_NP;
+
 class Utils {
 public:
+    // Constructors
     Utils();
     Utils(int amountOfRam, std::string typeOfRam);
     Utils(int amountOfRam, std::string typeOfRam, char unitOfRam);
+
+    // Misc
     int rand(int lb, int ub);
+
+    // Output
+    void loadingRAMAnim();
+
+
 private:
-    int amountOfRam{};
+//    int amountOfRam{};
     std::string typeOfRam;
-    char unitOfRam{};
+//    char unitOfRam{};
     std::default_random_engine randEngine;
 //    std::uniform_int_distribution<int> randDist; // Look at this later
 };
